3a1a0915a214f2ccf231752bbc9ab18f4be35129
Alice Chang <alicechang994@gmail.com>
2024-03-13 14:12:04 -0700

    added keyboard event listeners

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 /*
-to add:
-keyboard event listeners
-running list of past calculations at the top of the display screen
-
 ==========
 DOM Setup
 ==========
@@ -64,7 +60,7 @@ let total = 0;
 let num1 = "";
 let num2 = "";
 let operator = "";
-let listOfOperators = ["+", "-", "x", "÷"];
+let listOfOperators = ["+", "-", "x", "÷", "*", "/"];
 let calculationStatus = "calculating first pair";
 let resetNum2Status = false;
 let pastCalculations = [];
@@ -88,9 +84,9 @@ function operate(n1, n2, o) {
     add(n1, num2);
   } else if (o === "-") {
     subtract(n1, n2);
-  } else if (operator === "x") {
+  } else if (o === "x" || o === "*") {
     multiply(n1, n2);
-  } else if (o === "÷") {
+  } else if (o === "÷" || o === "/") {
     divide(n1, n2);
   }
   return Number(total.toFixed(2)); //rounds to 2 decimal places
@@ -122,29 +118,45 @@ function resetDisplayForNum2() {
   displayScreen.textContent = "";
 }
 
-function performCalculatorOperations(button) {
+//logic for operating the calculator
+function simulateClick(button) {
   const buttonText = button.textContent;
-  if (buttonText === ".") {
+  const keyedButton = button;
+  if (buttonText === "." || keyedButton === ".") {
     if (!(displayScreen.textContent.includes("."))) {
-      displayScreen.textContent += buttonText;
+      if (buttonText !== undefined) {
+        displayScreen.textContent += buttonText;
+      } else {
+        displayScreen.textContent += keyedButton;
+      }
     }
 
-  } else if (!isNaN(Number(buttonText))) {
+  } else if (!isNaN(Number(buttonText)) || !isNaN(Number(keyedButton))) {
     //if button selected is a number
 
     if (calculationStatus === "calculating first pair") {
-      displayScreen.textContent += buttonText;
+      if (buttonText !== undefined) {
+        displayScreen.textContent += buttonText;
+      } else {
+        displayScreen.textContent += keyedButton;
+      }
+
     } else if (calculationStatus === "first total calculated") {
       if (num2 === "" && resetNum2Status === false) {
         resetDisplayForNum2();
         resetNum2Status = true;
       }
-      displayScreen.textContent += buttonText;
+      if (buttonText !== undefined) {
+        displayScreen.textContent += buttonText;
+      } else {
+        displayScreen.textContent += keyedButton;
+      }
+
     }
-  } else if (buttonText === "+/-" && displayScreen.textContent !== "") {
+  } else if ((buttonText === "+/-" || keyedButton === "±") && displayScreen.textContent !== "") {
     displayScreen.textContent = Number(displayScreen.textContent) * -1;
     pastCalculations.push("+/-");
-  } else if (buttonText === "%" && displayScreen.textContent !== "") {
+  } else if ((buttonText === "%" || keyedButton === "%") && displayScreen.textContent !== "") {
     const percentVal =
       parseFloat(displayScreen.textContent.replace("%", "")) / 100;
     displayScreen.textContent = percentVal.toFixed(3);
@@ -163,18 +175,24 @@ function performCalculatorOperations(button) {
       }
     }
     //assign the operator
-    if (listOfOperators.includes(buttonText)) {
-      operator = buttonText;
+    if (listOfOperators.includes(buttonText) || listOfOperators.includes(keyedButton)) {
+
+      if (buttonText !== undefined) {
+        operator = buttonText;
+      } else {
+        operator = keyedButton;
+      }
+
       pastCalculations.push(operator);
       displayScreen.textContent = "";
     }
-    if (buttonText === "=") {
+    if (buttonText === "=" || keyedButton === "Enter" || keyedButton === "=") {
       console.log(num1, num2);
       total = operate(num1, num2, operator);
       pastCalculations.push("=");
       resetDisplay();
     }
-    if (buttonText === "AC") {
+    if (buttonText === "AC" || keyedButton === "Escape" || keyedButton === "Backspace") {
       allClear();
     }
   }
@@ -190,9 +208,15 @@ Event Listeners
 function populateDisplay() {
   const calcButtons = document.querySelectorAll(".calc-button");
 
+  //keydown event listener
+  document.addEventListener("keydown", function (event) {
+    simulateClick(event.key);
+  });
+
+  //mouse click event listener
   for (const button of calcButtons) {
     button.addEventListener("click", function () {
-      performCalculatorOperations(button);
+      simulateClick(button);
     });
   }
 }
